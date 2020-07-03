2190a68aea1e9931d7aafe7c7982a62e98cdca5f
n-is <nschl.npl@gmail.com>
2020-07-03 10:24:04 +0545

    Add example for: users | filter age > 30 and age < 50 | count by last_name.

---
+++ examples/csv.readusers/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/raralabs/canal/core/message"
+	"github.com/raralabs/canal/core/pipeline"
+	"github.com/raralabs/canal/core/transforms/agg"
+	"github.com/raralabs/canal/core/transforms/do"
+	"github.com/raralabs/canal/core/transforms/event/poll"
+	"github.com/raralabs/canal/ext/transforms/aggregates"
+	"github.com/raralabs/canal/ext/transforms/doFn"
+	"github.com/raralabs/canal/utils/cast"
+
+	csvsnk "github.com/raralabs/hike/plugins/canal/sinks"
+	csvsrc "github.com/raralabs/hike/plugins/canal/sources"
+	"github.com/raralabs/hike/utils/faker"
+)
+
+const TmpPath = "./tmp/"
+
+func main() {
+	if err := os.MkdirAll(TmpPath, os.ModePerm); err != nil {
+		log.Panic(err)
+	}
+
+	p := pipeline.NewPipeline(1)
+	opts := pipeline.DefaultProcessorOptions
+
+	filename := TmpPath + "users.csv"
+	generateCsv(filename)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	src := p.AddSource("CSV Reader")
+	sp := src.AddProcessor(opts, csvsrc.NewCsvReader(file, -1))
+
+	delay := p.AddTransform("Delay")
+	del := delay.AddProcessor(opts, doFn.DelayFunction(10*time.Millisecond), "path1")
+
+	ageFilter := func(m message.Msg, proc pipeline.IProcessorForExecutor) bool {
+
+		content := m.Content()
+
+		if v, ok := content["eof"]; ok {
+			if v.Val == true {
+				proc.Result(m, content)
+				proc.Done()
+				return false
+			}
+		}
+
+		rawAge := content["age"].Val
+		if age, ok := cast.TryFloat(rawAge); ok {
+			if age > 30 && age < 50 {
+				proc.Result(m, content)
+			}
+		}
+
+		return false
+	}
+	filt := p.AddTransform("Age Filter")
+	f1 := filt.AddProcessor(opts, do.NewOperator(ageFilter), "path2")
+
+	// Count Last Names
+	count := aggregates.NewCount("Count", func(m map[string]interface{}) bool {
+		return true
+	})
+	aggs := []agg.IAggregator{count}
+	filterEvent := poll.NewFilterEvent(func(m map[string]interface{}) bool {
+		return true
+	})
+	after := func(m message.Msg, proc pipeline.IProcessorForExecutor, msgs []message.MsgContent) bool {
+		content := m.Content()
+
+		if v, ok := content["eof"]; ok {
+			if v.Val == true {
+				for _, msg := range msgs {
+					proc.Result(m, msg)
+				}
+				return true
+			}
+		}
+		return false
+	}
+	aggregator := agg.NewAggregator(filterEvent, aggs, after, "last_name")
+
+	counter := p.AddTransform("Aggregator")
+	cnt := counter.AddProcessor(opts, aggregator.Function(), "path3")
+
+	snk := p.AddSink("CSV Writer")
+	snk.AddProcessor(opts, csvsnk.NewCsvWriter(os.Stdout), "sink")
+
+	delay.ReceiveFrom("path1", sp)
+	filt.ReceiveFrom("path2", del)
+	counter.ReceiveFrom("path3", f1)
+	snk.ReceiveFrom("sink", cnt)
+
+	c, cancel := context.WithTimeout(context.Background(), 1000*time.Second)
+	p.Validate()
+	aggregator.Start()
+	p.Start(c, cancel)
+
+	file.Close()
+}
+
+func generateCsv(filename string) {
+
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Panic(err)
+	}
+	// Generate Fake CSV Data
+	choices := map[string][]interface{}{
+		"first_name": {"Madhav", "Shambhu", "Pushpa", "Kumar", "Hero"},
+		"last_name":  {"Mashima", "Dahal", "Poudel", "Rimal", "Amatya", "Shrestha", "Bajracharya"},
+		"age":        {10, 20, 30, 40, 50, 60, 70, 15, 25, 35, 45, 55, 65, 75, 100, 6, 33, 47},
+	}
+	faker.FakeCsvData(file, choices, 10)
+	file.Close()
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/raralabs/hike
 
 go 1.13
 
-require github.com/raralabs/canal v0.0.0-20200702054059-47e669e82c32
+require github.com/raralabs/canal v0.0.0-20200703035747-4453a1090ae8
