1a4be8c676a530a56889490a83eb4636ded984d1
n-is <nschl.npl@gmail.com>
2020-07-07 12:15:24 +0545

    Update cli test example.

---
+++ examples/cli/main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/c-bata/go-prompt"
+	"github.com/raralabs/canal/utils/cast"
+	"github.com/raralabs/hike/parser/peg"
+	"github.com/raralabs/hike/utils/faker"
+	"log"
+	"os"
+	"time"
+)
+
+func basicCompleter(d prompt.Document) []prompt.Suggest {
+
+	s := []prompt.Suggest{
+		{Text: "faker", Description: "Generate fake csv file mode"},
+		{Text: "log", Description: "Starts log mode"},
+		{Text: "cmd", Description: "Starts command mode"},
+		{Text: "end", Description: "Ends current mode"},
+	}
+	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
+}
+
+func cmdCompleter(d prompt.Document) []prompt.Suggest {
+
+	s := []prompt.Suggest{
+		{Text: "file(", Description: "Add a file source or sink"},
+		{Text: "filter(", Description: "Add filter transform"},
+		{Text: "count(", Description: "Add count aggregator"},
+		{Text: "max(", Description: "Add max aggregator"},
+		{Text: "min(", Description: "Add min aggregator"},
+		{Text: "stdout(", Description: "Add stdout sink"},
+		{Text: "end", Description: "Ends current mode"},
+	}
+	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
+}
+
+func noneCompleter(d prompt.Document) []prompt.Suggest {
+	return []prompt.Suggest{}
+}
+
+func main() {
+	fmt.Println("Please enter command to run.")
+	
+	var closeFuncs []func()
+
+	builder := peg.NewPegCmd()
+	// Initialize the builder
+	builder.Init()
+
+	for {
+		t := prompt.Input("> ", basicCompleter)
+
+		switch t {
+		case "cmd":
+			// Command Mode
+			for {
+				cmd := prompt.Input("Cmd>> ", cmdCompleter)
+				if cmd == "end" {
+					break
+				}
+
+				// Build the command
+				starter, ppln, closer := builder.Build(1, cmd)
+
+				// Create context to run the pipeline
+				c, cancel := context.WithTimeout(context.Background(), 1000*time.Second)
+
+				// Run the starter to initialize all stages of a pipeline
+				starter()
+
+				// Run the pipeline
+				ppln.Start(c, cancel)
+
+				// Close everything
+				closer()
+			}
+
+		case "faker":
+			// Fake Data Generation Mode
+			fileName := prompt.Input("Filename>> ", noneCompleter)
+			numRows := prompt.Input("NumRows>> ", noneCompleter)
+			if rows, ok := cast.TryInt(numRows); ok {
+				generateCsv(fileName, int(rows))
+				fmt.Println("Fake csv file generation success.")
+			} else {
+				fmt.Println("Fake csv file generation failed.")
+			}
+
+		case "log":
+			// Log File Config Mode
+			fileName := prompt.Input("Log Filename>> ", noneCompleter)
+			closer := setLogFile(fileName)
+			fmt.Println("Log file config success.")
+
+			closeFuncs = append(closeFuncs, closer)
+		}
+
+		if t == "end" {
+			break
+		}
+	}
+
+	for _, fn := range closeFuncs {
+		fn()
+	}
+}
+
+func setLogFile(filename string) func() {
+	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+
+	log.SetOutput(f)
+
+	return func() {
+		f.Close()
+	}
+}
+
+func generateCsv(filename string, rows int) {
+
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Panic(err)
+	}
+	// Generate Fake CSV Data
+	choices := map[string][]interface{}{
+		"first_name": {"Madhav", "Shambhu", "Pushpa", "Kumar", "Hero"},
+		"last_name":  {"Mashima", "Dahal", "Poudel", "Rimal", "Amatya", "Shrestha", "Bajracharya"},
+		"age":        {10, 20, 30, 40, 50, 60, 70, 15, 25, 35, 45, 55, 65, 75, 100, 6, 33, 47},
+	}
+	faker.FakeCsvData(file, choices, rows)
+	file.Close()
+
+	log.Println("CSV File Generation Complete")
+	log.Printf("Total Rows: \t%v", rows)
+}
