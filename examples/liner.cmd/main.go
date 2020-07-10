35425974abf2c7fc44255cd0634ae1c589fb072b
n-is <nschl.npl@gmail.com>
2020-07-11 00:21:19 +0545

    Add hike cli using liner.

---
+++ examples/liner.cmd/main.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"fmt"
+	"github.com/peterh/liner"
+	"github.com/raralabs/hike/cli"
+	"github.com/raralabs/hike/parser/peg"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	historyFile = filepath.Join("./", ".cmd_history")
+
+	defaultSuggestions = []string{
+		"cmd",
+		"faker",
+		"log",
+		"end",
+	}
+
+	commandSuggestions = []string{
+		"file(",
+
+		"filter(",
+		"select(",
+		"head(",
+		"tail(",
+
+		"count(",
+		"dcount(",
+		"max(",
+		"min(",
+		"avg(",
+		"sum(",
+		"var(",
+		"median(",
+		"quartile(",
+		"quantile(",
+
+		"end",
+	}
+)
+
+func noneSuggest(line string) (c []string) {
+	return
+}
+
+func defaultSuggest(line string) (c []string) {
+	for _, n := range defaultSuggestions {
+		if strings.HasPrefix(n, strings.ToLower(line)) {
+			c = append(c, n)
+		}
+	}
+	return
+}
+
+func commandSuggest(line string) (c []string) {
+	for _, n := range commandSuggestions {
+		if strings.HasPrefix(n, strings.ToLower(line)) {
+			c = append(c, n)
+		}
+	}
+	return
+}
+
+func main() {
+	line := liner.NewLiner()
+	defer line.Close()
+
+	var closeFuncs []func()
+	defer func() {
+		for _, fn := range closeFuncs {
+			fn()
+		}
+
+		if f, err := os.Create(historyFile); err != nil {
+			log.Print("Error writing history file: ", err)
+		} else {
+			line.WriteHistory(f)
+			f.Close()
+		}
+	}()
+
+	line.SetCtrlCAborts(true)
+
+	if f, err := os.Open(historyFile); err == nil {
+		line.ReadHistory(f)
+		f.Close()
+	}
+
+	prsr := peg.NewPegCmd()
+
+	// Initialize the parser
+	prsr.Init()
+
+	// Set the log file
+	t, err := line.Prompt("Set Log File?(y/n) ")
+	if err == liner.ErrPromptAborted {
+		fmt.Println("Aborted")
+		return
+	} else if err != nil {
+		log.Panic(err)
+	}
+	
+	if strings.ToLower(t) == "y" {
+		closer := cli.LogMode(func(prompt string) string {
+			t, err := line.Prompt(prompt)
+			if err != nil {
+				log.Panic(err)
+			}
+			return t
+		})
+		closeFuncs = append(closeFuncs, closer)
+	} else {
+		cli.SetLogFile("default.log")
+		fmt.Println("Default logging to: default.log")
+	}
+
+	hikePrompt := func() {
+		defaultSuggestionsSet := false
+		for {
+			if !defaultSuggestionsSet {
+				defaultSuggestionsSet = true
+				line.SetCompleter(defaultSuggest)
+			}
+
+			t, err := line.Prompt("> ")
+			if err == liner.ErrPromptAborted {
+				fmt.Println("Aborted")
+				return
+			} else if err != nil {
+				log.Panic(err)
+			}
+
+			if t == "end" {
+				fmt.Println("Aborted")
+				break
+			}
+
+			switch t {
+			case "cmd":
+				// Command Mode
+				defaultSuggestionsSet = false
+				line.SetCompleter(commandSuggest)
+
+				cli.CommandMode(func(prompt string) string {
+					t, err := line.Prompt(prompt)
+					if err != nil {
+						log.Panic(err)
+					}
+					return t
+				}, func(cmd string) {
+					line.AppendHistory(strings.TrimSpace(cmd))
+				}, prsr)
+
+			case "faker":
+				// Fake Data Generation Mode
+				defaultSuggestionsSet = false
+				line.SetCompleter(noneSuggest)
+				cli.FakerMode(func(prompt string) string {
+					t, err := line.Prompt(prompt)
+					if err != nil {
+						log.Panic(err)
+					}
+					return t
+				})
+
+			case "log":
+				// Log File Config Mode
+				defaultSuggestionsSet = false
+				line.SetCompleter(noneSuggest)
+				closer := cli.LogMode(func(prompt string) string {
+					t, err := line.Prompt(prompt)
+					if err != nil {
+						log.Panic(err)
+					}
+					return t
+				})
+				closeFuncs = append(closeFuncs, closer)
+
+			default:
+				log.Panic("Wrong Command!!")
+			}
+		}
+	}
+
+	for {
+		if err := cli.HandlePanic(hikePrompt); err == nil {
+			break
+		} else {
+			fmt.Printf("Error: %v\n", err)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -9,7 +9,9 @@ require (
 	github.com/gizak/termui/v3 v3.1.0
 	github.com/go-openapi/strfmt v0.19.5 // indirect
 	github.com/jedib0t/go-pretty v4.3.0+incompatible
+	github.com/manifoldco/promptui v0.7.0
 	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/mattn/go-tty v0.0.3 // indirect
+	github.com/peterh/liner v1.2.0
 	github.com/raralabs/canal v0.0.0-20200710110246-0925efc5a855
 )
