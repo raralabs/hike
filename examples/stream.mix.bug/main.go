49bf400fd8b84d15c99c1a445242a6dad9923624
Manoj Ghimire <ghimire.manoj@gmail.com>
2020-08-26 17:02:48 +0545

    refactored

--- examples/stream.mix.bug/main.go
+++ examples/stream.mix.bug/main.go
@@ -5,11 +5,10 @@ import (
 	"github.com/Knetic/govaluate"
 	"github.com/raralabs/canal/core/message"
 	"github.com/raralabs/canal/core/pipeline"
+	canalSnk "github.com/raralabs/canal/ext/sinks"
 	"github.com/raralabs/canal/ext/sources"
 	"github.com/raralabs/canal/ext/transforms/doFn"
-	"github.com/raralabs/hike/plugins/canal/sinks"
 	"log"
-	"os"
 	"time"
 )
 
@@ -31,8 +30,8 @@ func main() {
 		return false
 	}
 
-	src := p.AddSource("Fake Source")
-	sp := src.AddProcessor(opts, sources.NewFaker(10, nil))
+	src := p.AddSource("Fake Source").Trace()
+	sp := src.AddProcessor(opts, sources.NewFaker(1, nil))
 
 	tr1 := p.AddTransform("Filter Stage")
 	pr1 := tr1.AddProcessor(opts, doFn.FilterFunction(func(m map[string]interface{}) (bool, error) {
@@ -48,11 +47,11 @@ func main() {
 	pr2 := tr2.AddProcessor(opts, doFn.EnrichFunction("twice_age", expr, doneFunc), "path2")
 
 	snk1 := p.AddSink("Filter Sink")
-	snk1.AddProcessor(opts, sinks.NewPrettyPrinter(os.Stdout, 10), "path3")
+	snk1.AddProcessor(opts, canalSnk.NewStdoutSink(), "path3")
 	//snk1.AddProcessor(opts, canalSnk.NewBlackholeSink(), "path3")
 
 	snk2 := p.AddSink("Enrich Sink")
-	snk2.AddProcessor(opts, sinks.NewPrettyPrinter(os.Stdout, 5), "path4")
+	snk2.AddProcessor(opts, canalSnk.NewStdoutSink(), "path4")
 	//snk2.AddProcessor(opts, canalSnk.NewBlackholeSink(), "path4")
 
 	tr1.ReceiveFrom("path1", sp)
