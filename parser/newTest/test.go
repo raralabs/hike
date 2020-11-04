f8393cb2e1c49bce9bd0efe6bda14355c55858df
subodh7591 <subshakya591@gmail.com>
2020-11-04 16:08:45 +0545

    minor modifications on idiom.peg file

--- parser/newTest/test.go
+++ parser/newTest/test.go
@@ -6,13 +6,23 @@ import (
 	"github.com/raralabs/hike/utils/LanguageProcessor"
 )
 
-func main(){
-	cmd:=`file(something.csv) | filter(age>30) | filter(age>20) | p1 = into();
-          file(anything.csv) | filter(age > 30) | stdout();
-          branch(b1) | filter(age > 30) | stdout();;`
 
+func IsComment(statement string)bool{
+	if statement[0]=='#'{
+		return true
+	}
+	return false
+}
+
+func main(){
+	cmd:=`file(anything.csv) | filter(age>30) | filter(age>30) | b1 = into();
+          branch(b1) | agg counter = count() | stdout();
+          file(anything.csv) | filter(age>30) | b1 = into();
+          branch(b1) | filter(age>30) | stdout();;`
+
+	//remove comments from command
+	//filteredCommand := newPeg.RemoveStatement(cmd,IsComment)
 	cleanedCommand := LanguageProcessor.PrepareQuery(cmd)
-	fmt.Println("cleaned",cleanedCommand)
 	stg,err := newPeg.Parse("", []byte(cleanedCommand))
 
 	fmt.Println(stg,err)
